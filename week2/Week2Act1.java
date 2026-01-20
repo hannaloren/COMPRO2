d59721875bf9067c5f59ab1259453008ab396aec
hannaloren <hannaloren.obra@lorma.edu>
2026-01-20 20:13:51 +0800

    theater seat problem pt 1

--- week2/Week2Act1.java
+++ week2/Week2Act1.java
@@ -1,13 +1,9 @@
 package week2;
-<<<<<<< HEAD
-=======
 
->>>>>>> 0ea658ae75b22082966368c9c7b363f16328ae69
 public class Week2Act1 {
     public static void main(String[] args) {
-        int [] theaterRow = new int [8];
-        theaterRow[3] =  1;
-<<<<<<< HEAD
+        int[] theaterRow = new int[8];
+        theaterRow[3] = 1;
         theaterRow[5] = 1;
         theaterRow[2] = 1;
         theaterRow[7] = 1;
@@ -27,24 +23,12 @@ public class Week2Act1 {
         int status = 0;
         for (int seat = 0; seat < theaterRow.length; seat++) {
             if (seat != theaterRow[seat]) {
-=======
-
-        System.out.println("Theater Row Seat Status:");
-        for (int i = 0; i < theaterRow.length; i++) {
-            System.out.println("Seat " + i + ": " + (theaterRow[i] == 0 ? "Available" : "Booked"));
-        }
-
-        int status = 0;
-        for (int seat = 0; seat < theaterRow.length; seat++) {
-            if (seat != 3){
->>>>>>> 0ea658ae75b22082966368c9c7b363f16328ae69
                 status++;
-            }   
+            }
         }
         System.out.println();
         System.out.println("Seat available: " + status);
     }
-<<<<<<< HEAD
 
     public static void printRow(int[] row) {
         for (int seat : row) {
@@ -53,7 +37,3 @@ public class Week2Act1 {
         System.out.println();
     }
 }
-
-=======
-}
->>>>>>> 0ea658ae75b22082966368c9c7b363f16328ae69
