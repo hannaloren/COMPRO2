c46202a122c30b3f7d4e4eadfd804149816cc35e
hannaloren <hannaloren.obra@lorma.edu>
2026-01-29 12:55:39 +0800

    theater seating pt 1

--- activity1/TheaterSeating.java
+++ activity1/TheaterSeating.java
@@ -4,22 +4,14 @@ public class TheaterSeating {
     public static void main(String[] args) {
         int[] theaterRow = new int[8];
         theaterRow[3] = 1;
-        theaterRow[5] = 1;
-        theaterRow[2] = 1;
-        theaterRow[7] = 1;
-        theaterRow[4] = 1;
 
-        System.out.println("Theater Row Seat Status:");
-        printRow(theaterRow);
-        printRow(theaterRow);
-        printRow(theaterRow);
-        printRow(theaterRow);
-        printRow(theaterRow);
-
-        seatAvailable(theaterRow);
-    }
-
-    public static void seatAvailable(int[] theaterRow) {
+        System.out.println("Theater Row Seat Status (0 = Available, 1= Booked)");
+        System.out.println();
+        int[] row = theaterRow;
+        for (int seat : row) {
+            System.out.print(seat == 0 ? " |-| " : " |1| ");
+        }
+        System.out.println();
         int status = 0;
         for (int seat = 0; seat < theaterRow.length; seat++) {
             if (seat != theaterRow[seat]) {
@@ -28,12 +20,7 @@ public class TheaterSeating {
         }
         System.out.println();
         System.out.println("Seat available: " + status);
+
     }
 
-    public static void printRow(int[] row) {
-        for (int seat : row) {
-            System.out.print(seat == 0 ? "|-|" : "|x|");
-        }
-        System.out.println();
-    }
 }
