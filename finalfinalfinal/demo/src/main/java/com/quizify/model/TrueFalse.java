d9c55aa7c6030c6f51bdb49500cf9aec4ab8f7ea
hannaloren <hannaloren.obra@lorma.edu>
2026-05-17 16:31:01 +0800

    finished on 5/15/2026

--- finalfinalfinal/demo/src/main/java/com/quizify/model/TrueFalse.java
+++ finalfinalfinal/demo/src/main/java/com/quizify/model/TrueFalse.java
@@ -6,7 +6,6 @@ public class TrueFalse extends Question {
     public TrueFalse(String prompt, String correctAnswer) {
         super(prompt, correctAnswer);
     }
-
     @Override
     public void display() {
         System.out.println("Question Type: True/False");
